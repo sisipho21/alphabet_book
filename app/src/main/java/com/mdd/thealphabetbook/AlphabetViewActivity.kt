06592844306e59b6c0a7f261e94f58d89b6f4533
Sisipho Tukushe <tkssis001@sl-dual-237.cs.uct.ac.za>
2022-09-16 21:43:55 +0200

    All buttons, except 2, work as expected

--- app/src/main/java/com/mdd/thealphabetbook/AlphabetViewActivity.kt
+++ app/src/main/java/com/mdd/thealphabetbook/AlphabetViewActivity.kt
@@ -1,51 +1,65 @@
 package com.mdd.thealphabetbook
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import com.mdd.thealphabetbook.MainActivity.HelpClickListener
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class AlphabetViewActivity : AppCompatActivity() {
+    var arrPictures = arrayOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alphabet_view)
+        val intent = intent
+        val value = intent.getIntExtra("intName", 0)
 
         val overviewBtn = findViewById<Button>(R.id.button_overview)
         val firstBtn = findViewById<Button>(R.id.button_first)
         val lastBtn = findViewById<Button>(R.id.button_last)
+        val imgView = findViewById<ImageView>(R.id.imageView)
 
-        val helpClickListener = HelpClickListener()
+        val helpClickListener = AlphabetHelpClickListener(imgView)
 
         overviewBtn.setOnClickListener(helpClickListener)
         firstBtn.setOnClickListener(helpClickListener)
         lastBtn.setOnClickListener(helpClickListener)
 
+        arrPictures = arrayOf<Int>(R.drawable.slide01, R.drawable.slide02, R.drawable.slide03, R.drawable.slide04, R.drawable.slide05, R.drawable.slide06, R.drawable.slide07, R.drawable.slide08, R.drawable.slide09,
+            R.drawable.slide10, R.drawable.slide11, R.drawable.slide12, R.drawable.slide13, R.drawable.slide14, R.drawable.slide15, R.drawable.slide16, R.drawable.slide17,
+            R.drawable.slide18, R.drawable.slide19, R.drawable.slide20, R.drawable.slide21, R.drawable.slide22, R.drawable.slide23, R.drawable.slide24, R.drawable.slide25, R.drawable.slide26)
 
+        changeImage(imgView, value)
+
+    }
+
+    fun changeImage(view: ImageView, num: Int){
+        view.setImageResource(arrPictures[num])
     }
 
     fun nextActivity(){
         val intentAlphabet = Intent(this, MainActivity::class.java)
         startActivity(intentAlphabet)
-
     }
 
-    inner class HelpClickListener : View.OnClickListener{
+    inner class AlphabetHelpClickListener (val imageView: ImageView): View.OnClickListener{
         override fun onClick(p0: View?) {
 
             val view = p0?.rootView
             when (p0?.id) {
 
                 R.id.button_overview -> {
-
                     nextActivity()
                 }
                 R.id.button_first -> {
-                    nextActivity()
+                    changeImage(imageView, 0)
                 }
                 R.id.button_last -> {
-                    nextActivity()
+                    changeImage(imageView, 25)
                 }
 
             }
--- app/src/main/java/com/mdd/thealphabetbook/MainActivity.kt
+++ app/src/main/java/com/mdd/thealphabetbook/MainActivity.kt
@@ -6,49 +6,152 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
-class MainActivity : AppCompatActivity(){
+open class MainActivity : AppCompatActivity(){
 
-//val arrBtns = arrayOf<Button>(findViewById<Button>(R.id.button4), findViewById<Button>(R.id.button5), findViewById<Button>(R.id.button6))
+    val helperListener = MainHelpClickListener()
+    var arrBtn = arrayOf<Button>()
+    var btnValue = 0
+    var str ="value"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonA = findViewById<Button>(R.id.button4)
-        val buttonB = findViewById<Button>(R.id.button5)
-        val buttonC = findViewById<Button>(R.id.button6)
-        val buttonD = findViewById<Button>(R.id.button7)
-        val buttonE = findViewById<Button>(R.id.button8)
+        val buttonA = findViewById<Button>(R.id.btn_a)
+        val buttonB = findViewById<Button>(R.id.btn_b)
+        val buttonC = findViewById<Button>(R.id.btn_c)
+        val buttonD = findViewById<Button>(R.id.btn_d)
+        val buttonE = findViewById<Button>(R.id.btn_e)
+        val buttonF = findViewById<Button>(R.id.btn_f)
+        val buttonG = findViewById<Button>(R.id.btn_g)
+        val buttonH = findViewById<Button>(R.id.btn_h)
+        val buttonI = findViewById<Button>(R.id.btn_i)
+        val buttonJ = findViewById<Button>(R.id.btn_j)
+        val buttonK = findViewById<Button>(R.id.btn_k)
+        val buttonL = findViewById<Button>(R.id.btn_l)
+        val buttonM = findViewById<Button>(R.id.btn_m)
+        val buttonN = findViewById<Button>(R.id.btn_n)
+        val buttonO = findViewById<Button>(R.id.btn_o)
+        val buttonP = findViewById<Button>(R.id.btn_p)
+        val buttonQ = findViewById<Button>(R.id.btn_q)
+        val buttonR = findViewById<Button>(R.id.btn_r)
+        val buttonS = findViewById<Button>(R.id.btn_s)
+        val buttonT = findViewById<Button>(R.id.btn_t)
+        val buttonU = findViewById<Button>(R.id.btn_u)
+        val buttonV = findViewById<Button>(R.id.btn_v)
+        val buttonW = findViewById<Button>(R.id.btn_w)
+        val buttonX = findViewById<Button>(R.id.btn_x)
+        val buttonY = findViewById<Button>(R.id.btn_y)
+        val buttonZ = findViewById<Button>(R.id.btn_z)
 
-        val helpClickListener = HelpClickListener()
+        arrBtn = arrayOf<Button>(buttonA, buttonB, buttonC, buttonD, buttonE, buttonF, buttonG, buttonH, buttonI, buttonJ, buttonK, buttonL,
+            buttonM, buttonN, buttonO, buttonP, buttonQ, buttonR, buttonS, buttonT, buttonU, buttonV, buttonW, buttonX, buttonY, buttonZ)
 
+        activateListener(arrBtn)
+    }
 
-        buttonA.setOnClickListener(helpClickListener)
-        buttonB.setOnClickListener(helpClickListener)
-        buttonC.setOnClickListener(helpClickListener)
-        buttonD.setOnClickListener(helpClickListener)
-        buttonE.setOnClickListener(helpClickListener)
-
+    fun activateListener(array: Array<Button>){
+        for (x in array){
+            x.setOnClickListener(helperListener)
+        }
     }
 
     fun nextActivity(){
-        val intentAlphabet = Intent(this, AlphabetViewActivity::class.java)
+        val intentAlphabet = Intent( this, AlphabetViewActivity::class.java)
+        intentAlphabet.putExtra("intName",btnValue)
         startActivity(intentAlphabet)
-
     }
 
-    inner class HelpClickListener : View.OnClickListener{
+
+
+    inner class MainHelpClickListener : View.OnClickListener, AppCompatActivity(){
+        /** This inner class works as a controller between the view(Main Activity) and the model (AlphabetView Activity)*/
+
         override fun onClick(p0: View?) {
 
             val view = p0?.rootView
             when (p0?.id) {
 
-                R.id.button4, R.id.button5, R.id.button6, R.id.button7 -> {
-
-                    nextActivity()
+                R.id.btn_a ->{
+                    btnValue = 0
                 }
-                R.id.button8 -> {
-                    nextActivity()
+                R.id.btn_b ->{
+                    btnValue = 1
+                }
+                R.id.btn_c ->{
+                    btnValue = 2
+                }
+                R.id.btn_d ->{
+                    btnValue = 3
+                }
+                R.id.btn_e -> {
+                    btnValue = 4
+                }
+                R.id.btn_f ->{
+                    btnValue = 5
+                }
+                R.id.btn_g ->{
+                    btnValue = 6
+                }
+                R.id.btn_h ->{
+                    btnValue = 7
+                }
+                R.id.btn_i ->{
+                    btnValue = 8
+                }
+                R.id.btn_j -> {
+                    btnValue = 9
+                }
+                R.id.btn_k ->{
+                    btnValue = 10
+                }
+                R.id.btn_l ->{
+                    btnValue = 11
+                }
+                R.id.btn_m ->{
+                    btnValue = 12
+                }
+                R.id.btn_n ->{
+                    btnValue = 13
+                }
+                 R.id.btn_o -> {
+                    btnValue = 14
+                }
+                R.id.btn_p ->{
+                    btnValue = 15
+                }
+                R.id.btn_q ->{
+                    btnValue = 16
+                }
+                R.id.btn_r ->{
+                    btnValue = 17
+                }
+                R.id.btn_s ->{
+                    btnValue = 18
+                }
+                R.id.btn_t -> {
+                    btnValue = 19
+                }
+                R.id.btn_u ->{
+                    btnValue = 20
+                }
+                R.id.btn_v ->{
+                    btnValue = 21
+                }
+                R.id.btn_w ->{
+                    btnValue = 22
+                }
+                R.id.btn_x ->{
+                    btnValue = 23
+                }
+                R.id.btn_y ->{
+                    btnValue = 24
+                }
+                R.id.btn_z -> {
+                    btnValue = 25
+
                 }
             }
+            nextActivity()
         }
     }
 
